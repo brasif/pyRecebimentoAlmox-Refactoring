ec4279c12f91c87b2885a571e23c678a6dd05002
Vitor Oliveira <vitorsimon04@gmail.com>
2024-10-12 00:15:45 -0300

    FIX - Correção name Enum

--- RECEBIMENTO/models/enum_filiais.py
+++ RECEBIMENTO/models/enum_filiais.py
@@ -1,12 +1,12 @@
 from enum import Enum
 
 class Filiais(Enum):
-    JUNDIAÍ = "Jundiaí"
+    JUNDIAI = "Jundiaí"
     BELO_HORIZONTE = "Belo Horizonte"
-    RIBEIRÃO_PRETO = "Ribeirão Preto"
-    CUIABÁ = "Cuiabá"
+    RIBEIRAO_PRETO = "Ribeirão Preto"
+    CUIABA = "Cuiabá"
     RIO_DE_JANEIRO = "Rio de Janeiro"
     TOCANTINS = "Tocantins"
-    BRASÍLIA = "Brasília"
-    GOIÂNIA = "Goiânia"
+    BRASILIA = "Brasília"
+    GOIANIA = "Goiânia"
     CURITIBA = "Curitiba"
