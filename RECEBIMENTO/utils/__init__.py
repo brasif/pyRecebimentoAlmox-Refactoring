d9490e72b7d32be067475d3613210bfaa0883154
Vitor Oliveira <vitorsimon04@gmail.com>
2024-10-13 01:31:36 -0300

    FEAT - Dicionário de status em utils

--- RECEBIMENTO/utils/__init__.py
+++ RECEBIMENTO/utils/__init__.py
@@ -1,1 +1,2 @@
 from .autenticacao_utils import *
+from .registro_status_choices import *
---
+++ RECEBIMENTO/utils/registro_status_choices.py
@@ -0,0 +1,10 @@
+# Dicionário - REGISTRO_STATUS_CHOICES
+
+REGISTRO_STATUS_CHOICES = [
+    ("Pendência almoxarifado", "Pendência almoxarifado"),
+    ("Aguardando ajuste", "Aguardando ajuste"),
+    ("Aguardando fiscal", "Aguardando fiscal"),
+    ("Aguardando lançamento de CTE", "Aguardando lançamento de CTE"),
+    ("NF cancelada", "NF cancelada"),
+    ("NF finalizada", "NF finalizada")
+]
