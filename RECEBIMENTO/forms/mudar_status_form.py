63848d1c231a8edf1c6a01c8a6fa407f99b463f3
Vitor Oliveira <vitorsimon04@gmail.com>
2024-10-15 16:07:29 -0300

    FIX - Seleção do responsável associado a NF

--- RECEBIMENTO/forms/mudar_status_form.py
+++ RECEBIMENTO/forms/mudar_status_form.py
@@ -4,6 +4,11 @@ from wtforms.validators import DataRequired
 
 
 class MudarStatusForm(FlaskForm):
+    # Campo para selecionar o ID do responsável
+    id_responsavel = SelectField('Responsável', coerce=int, validators=[
+        DataRequired(message="Por favor, selecione um responsável.")
+    ])
+    
     # Status
     status = SelectField('Status', choices=[], validators=[
         DataRequired(message="Por favor, selecione um status.")
--- RECEBIMENTO/routes/mudar_status/mudar_status_routes.py
+++ RECEBIMENTO/routes/mudar_status/mudar_status_routes.py
@@ -1,9 +1,9 @@
-from flask import render_template, redirect, url_for, flash
+from flask import render_template, redirect, url_for, flash, request
 from RECEBIMENTO import db
 from sqlalchemy.exc import SQLAlchemyError
 from RECEBIMENTO.forms import MudarStatusForm
-from RECEBIMENTO.models import NotaFiscal, Registro
-from RECEBIMENTO.utils.status_registro import REGISTRO_STATUS_CHOICES
+from RECEBIMENTO.models import NotaFiscal, Registro, Responsavel, ResponsavelFilial
+from RECEBIMENTO.utils import obter_numero_nf, REGISTRO_STATUS_CHOICES
 from flask_login import login_required, current_user
 from . import mudar_status_bp
 
@@ -15,32 +15,62 @@ def registro_mudar_status(id_nota_fiscal):
     nota_fiscal = NotaFiscal.query.get_or_404(id_nota_fiscal)
     form = MudarStatusForm(obj=nota_fiscal)
     
+    ultimo_registro = Registro.query.filter_by(id_nota_fiscal=nota_fiscal.id_nota_fiscal).order_by(Registro.id_registro.desc()).first()
+    
+    if not ultimo_registro:
+        flash("Não foi possível obter o registro da nota fiscal", "danger")
+        return redirect(url_for("menu.menu"))
+    
     try:
+        # Busca os responsáveis vinculados à filial da NF
+        responsaveis_vinculados =(Responsavel.query
+            .join(ResponsavelFilial)
+            .filter(ResponsavelFilial.filial == nota_fiscal.filial, Responsavel.status == True)
+            .all()
+        )
+
+        if not responsaveis_vinculados:
+            flash("Nenhum responsável encontrado nas filiais vinculadas.", "warning")
+            form.id_responsavel.choices = []
+        else:
+            # Preencher choices com os responsáveis vinculados
+            form.id_responsavel.choices = [(0, "Selecione um responsável")] + [(resp.id_responsavel, resp.nome_responsavel) for resp in responsaveis_vinculados]
+            if request.method == 'GET':
+                form.id_responsavel.data = ultimo_registro.id_responsavel
+
         if not REGISTRO_STATUS_CHOICES:
             flash("Nenhum status encontrado. Por favor, abra um chamado para a T.I. para que o problema possa ser solucionado.", "danger")
             form.status.choices = []
         else:
-            form.status.choices = [("", "Selecione um status")] + [(status[0], status[1]) for status in REGISTRO_STATUS_CHOICES]
-
-
+            # Remove o último status registrado do choices
+            status_disponiveis = [status for status in REGISTRO_STATUS_CHOICES if status[0] != ultimo_registro.status_registro]
+            form.status.choices = [("", "Selecione um status")] + [(status[0], status[1]) for status in status_disponiveis]
+    
     except SQLAlchemyError as e:
         flash(f"Erro ao acessar o banco de dados ao carregar as empresas: {str(e)}", "danger")
+        form.id_responsavel.choices = []
         form.status.choices = []
 
     except Exception as e:
         flash(f"Erro inesperado ao carregar as opções: {str(e)}", "danger")
+        form.id_responsavel.choices = []
         form.status.choices = []
 
 
     if form.validate_on_submit():
         try:
-            # Cria registro com novo status para a nota fiscal
-            recebimento = Registro.criar_registro(form, id_nota_fiscal, current_user.id_responsavel, form.status.data)
-            db.session.add(recebimento)
-            db.session.commit()
+            # Verifica se o status do forms é o mesmo que o ultimo status registrado
+            if form.status.data == ultimo_registro.status_registro:
+                
+                # Cria registro com novo status para a nota fiscal
+                recebimento = Registro.criar_registro(form, id_nota_fiscal, current_user.id_responsavel, form.status.data)
+                db.session.add(recebimento)
+                db.session.commit()
 
-            flash('Registro atualizado com sucesso!', 'success')
-            return redirect(url_for('tabela.tabela_registros'))
+                flash('Registro atualizado com sucesso!', 'success')
+                return redirect(url_for('tabela.tabela_registros'))
+            else:
+                flash('Insira um status diferente do atual para continuar.', 'warning')
 
         except ValueError as ve:
             db.session.rollback()
@@ -54,4 +84,4 @@ def registro_mudar_status(id_nota_fiscal):
             db.session.rollback()
             flash(f"Erro inesperado: {str(e)}", "danger")
 
-    return render_template('/mudar_status/registro_mudar_status.html', form=form, nota_fiscal=nota_fiscal)
+    return render_template('/mudar_status/registro_mudar_status.html', form=form, nota_fiscal=nota_fiscal, status_atual=ultimo_registro.status_registro)
