f0ac92acb63c4fd0c1f8d0edd761d4a48872e45a
Vitor Oliveira <vitorsimon04@gmail.com>
2024-10-15 21:15:50 -0300

    FEAT - Validação nas rotas de operações

--- RECEBIMENTO/routes/estorno/estorno_routes.py
+++ RECEBIMENTO/routes/estorno/estorno_routes.py
@@ -1,8 +1,10 @@
+import werkzeug
 from flask import render_template, redirect, url_for, flash, request
 from RECEBIMENTO import db
 from sqlalchemy.exc import SQLAlchemyError
 from RECEBIMENTO.forms import EstornoForm
 from RECEBIMENTO.models import NotaFiscal, Registro, Responsavel, ResponsavelFilial
+from RECEBIMENTO.utils import operacao_estorno
 from flask_login import login_required
 from . import estorno_bp
 
@@ -12,64 +14,73 @@ from . import estorno_bp
 @login_required
 def registro_estorno(id_nota_fiscal):
     nota_fiscal = NotaFiscal.query.get_or_404(id_nota_fiscal)
-    form = EstornoForm(obj=nota_fiscal)
-    
-    ultimo_registro = Registro.query.filter_by(id_nota_fiscal=nota_fiscal.id_nota_fiscal).order_by(Registro.id_registro.desc()).first()
-    
-    if not ultimo_registro:
-        flash("Não foi possível obter o registro da nota fiscal", "danger")
-        return redirect(url_for("menu.menu"))
-    
-    try:
-        # Busca os responsáveis vinculados à filial da NF
-        responsaveis_vinculados =(Responsavel.query
-            .join(ResponsavelFilial)
-            .filter(ResponsavelFilial.filial == nota_fiscal.filial, Responsavel.status == True)
-            .all()
-        )
 
-        if not responsaveis_vinculados:
-            flash("Nenhum responsável encontrado nas filiais vinculadas.", "warning")
-            form.id_responsavel.choices = []
-        else:
-            # Preencher choices com os responsáveis vinculados
-            form.id_responsavel.choices = [(0, "Selecione um responsável")] + [(resp.id_responsavel, resp.nome_responsavel) for resp in responsaveis_vinculados]
-            if request.method == "GET":
-                form.id_responsavel.data = ultimo_registro.id_responsavel
+    # Valida a chave de acesso para a operação de mudança de status
+    validacao = operacao_estorno(nota_fiscal.chave_acesso)
     
-    except SQLAlchemyError as e:
-        flash(f"Erro ao acessar o banco de dados ao carregar as empresas: {str(e)}", "danger")
-        form.id_responsavel.choices = []
-
-    except Exception as e:
-        flash(f"Erro inesperado ao carregar as opções: {str(e)}", "danger")
-        form.id_responsavel.choices = []
-
-
-    if form.validate_on_submit():
+    # Verifica se a função retorna um redirecionamento
+    if isinstance(validacao, werkzeug.wrappers.Response):
+        return validacao
+    else:
+        # A execução continua apenas se não houver redirecionamento na função operacao_estorno
+        form = EstornoForm(obj=nota_fiscal)
+        
+        ultimo_registro = Registro.query.filter_by(id_nota_fiscal=nota_fiscal.id_nota_fiscal).order_by(Registro.id_registro.desc()).first()
+        
+        if not ultimo_registro:
+            flash("Não foi possível obter o registro da nota fiscal", "danger")
+            return redirect(url_for("menu.menu"))
+        
         try:
-            # Verifica se o ultimo status registrado é igual a "Estornado"
-            if "Estornado" != ultimo_registro.status_registro:
-                # Cria registro com novo status "Estornado" para a nota fiscal
-                recebimento = Registro.criar_registro(id_nota_fiscal, form.id_responsavel.data, "Estornado")
-                db.session.add(recebimento)
-                db.session.commit()
+            # Busca os responsáveis vinculados à filial da NF
+            responsaveis_vinculados =(Responsavel.query
+                .join(ResponsavelFilial)
+                .filter(ResponsavelFilial.filial == nota_fiscal.filial, Responsavel.status == True)
+                .all()
+            )
 
-                flash("Registro estornado com sucesso!", "success")
-                return redirect(url_for("tabela.tabela_registros"))
+            if not responsaveis_vinculados:
+                flash("Nenhum responsável encontrado nas filiais vinculadas.", "warning")
+                form.id_responsavel.choices = []
             else:
-                flash("Insira um status diferente do atual para continuar.", "warning")
-
-        except ValueError as ve:
-            db.session.rollback()
-            flash(str(ve), "warning")
-
+                # Preencher choices com os responsáveis vinculados
+                form.id_responsavel.choices = [(0, "Selecione um responsável")] + [(resp.id_responsavel, resp.nome_responsavel) for resp in responsaveis_vinculados]
+                if request.method == "GET":
+                    form.id_responsavel.data = ultimo_registro.id_responsavel
+        
         except SQLAlchemyError as e:
-            db.session.rollback()
-            flash(f"Erro ao acessar o banco de dados: {str(e)}", "danger")
+            flash(f"Erro ao acessar o banco de dados ao carregar as empresas: {str(e)}", "danger")
+            form.id_responsavel.choices = []
 
         except Exception as e:
-            db.session.rollback()
-            flash(f"Erro inesperado: {str(e)}", "danger")
+            flash(f"Erro inesperado ao carregar as opções: {str(e)}", "danger")
+            form.id_responsavel.choices = []
 
-    return render_template("/estorno/registro_estorno.html", form=form, nota_fiscal=nota_fiscal, status_atual=ultimo_registro.status_registro)
+
+        if form.validate_on_submit():
+            try:
+                # Verifica se o ultimo status registrado é igual a "Estornado"
+                if "Estornado" != ultimo_registro.status_registro:
+                    # Cria registro com novo status "Estornado" para a nota fiscal
+                    recebimento = Registro.criar_registro(id_nota_fiscal, form.id_responsavel.data, "Estornado")
+                    db.session.add(recebimento)
+                    db.session.commit()
+
+                    flash("Registro estornado com sucesso!", "success")
+                    return redirect(url_for("tabela.tabela_registros"))
+                else:
+                    flash("Insira um status diferente do atual para continuar.", "warning")
+
+            except ValueError as ve:
+                db.session.rollback()
+                flash(str(ve), "warning")
+
+            except SQLAlchemyError as e:
+                db.session.rollback()
+                flash(f"Erro ao acessar o banco de dados: {str(e)}", "danger")
+
+            except Exception as e:
+                db.session.rollback()
+                flash(f"Erro inesperado: {str(e)}", "danger")
+
+        return render_template("/estorno/registro_estorno.html", form=form, nota_fiscal=nota_fiscal, status_atual=ultimo_registro.status_registro)
--- RECEBIMENTO/routes/nota_fiscal_e_recebimento/criar_nota_fiscal__e_recebimento_routes.py
+++ RECEBIMENTO/routes/nota_fiscal_e_recebimento/criar_nota_fiscal__e_recebimento_routes.py
@@ -1,9 +1,10 @@
+import werkzeug
 from flask import render_template, redirect, url_for, flash
 from RECEBIMENTO import db
 from sqlalchemy.exc import SQLAlchemyError
 from RECEBIMENTO.forms import NotaFiscalRecebimentoForm
 from RECEBIMENTO.models import NotaFiscal, ResponsavelFilial, Responsavel, Registro, CENTROS_POR_FILIAL
-from RECEBIMENTO.utils import definicao_status_recebimento, obter_numero_nf
+from RECEBIMENTO.utils import operacao_recebimento, definicao_status_recebimento, obter_numero_nf
 from flask_login import login_required, current_user
 from . import nota_fiscal_bp
 
@@ -11,81 +12,90 @@ from . import nota_fiscal_bp
 @nota_fiscal_bp.route('/recebimento/cadastro/<string:chave_acesso>', methods=['GET', 'POST'])
 @login_required
 def criar_nota_fiscal(chave_acesso):
-    form = NotaFiscalRecebimentoForm()
+
+    # Valida a chave de acesso para a operação de mudança de status
+    validacao = operacao_recebimento(chave_acesso)
     
-    try:
-        # Busca as filiais associadas ao responsável logado
-        filiais_vinculadas = db.session.query(ResponsavelFilial).filter_by(id_responsavel=current_user.id_responsavel).all()
-
-        if not filiais_vinculadas:
-            flash("Nenhuma filial vinculada ao responsável.", "warning")
-            form.filial.choices = []
-        else:
-            form.filial.choices = [("", "Selecione uma filial")] + [(filial.filial.name, filial.filial.value) for filial in filiais_vinculadas]
-
-            # Lista de filiais vinculadas (extraindo o valor de 'filial' do enum)
-            filiais = [filial.filial for filial in filiais_vinculadas]
-
-            # Busca os responsáveis associados às mesmas filiais vinculadas ao responsável logado
-            responsaveis_vinculados = db.session.query(Responsavel).join(ResponsavelFilial).filter(ResponsavelFilial.filial.in_(filiais)).all()
-
-            if not responsaveis_vinculados:
-                flash("Nenhum responsável encontrado nas filiais vinculadas.", "warning")
-                form.id_responsavel.choices = []
-            else:
-                # Preencher choices com os responsáveis vinculados
-                form.id_responsavel.choices = [(0, "Selecione um responsável")] + [(resp.id_responsavel, resp.nome_responsavel) for resp in responsaveis_vinculados]
-
-        # Busca os centros
-        centros = [centro for centros in CENTROS_POR_FILIAL.values() for centro in centros]
-        if not centros:
-            form.nome_centro.choices = [("", "Selecione um centro")]
-        else:
-            form.nome_centro.choices = [("", "Selecione um centro")] + [(centro, centro) for centro in centros]
-
-    except SQLAlchemyError as e:
-        flash(f"Erro ao acessar o banco de dados ao carregar as empresas: {str(e)}", "danger")
-        form.nome_centro.choices = []
-        form.id_responsavel.choices = []
-        form.filial.choices = []
+    # Verifica se a função retorna um redirecionamento
+    if isinstance(validacao, werkzeug.wrappers.Response):
+        return validacao
+    else:
+        # A execução continua apenas se não houver redirecionamento na função operacao_recebimento
+        form = NotaFiscalRecebimentoForm()
         
-    except Exception as e:
-        flash(f"Erro inesperado ao carregar as opções: {str(e)}", "danger")
-        form.nome_centro.choices = []
-        form.id_responsavel.choices = []
-        form.filial.choices = []
-    
-
-    if form.validate_on_submit():
         try:
-            nova_nota_fiscal = NotaFiscal.criar_nota_fiscal(chave_acesso, form)
-            db.session.add(nova_nota_fiscal)
-            db.session.flush()  # Envia as mudanças para o banco para gerar o ID
-            
-            # Acessa o ID da nova nota fiscal corretamente
-            id_nota_fiscal = nova_nota_fiscal.id_nota_fiscal
-            
-            # Cria uma nova instância de Registro
-            status = definicao_status_recebimento(form.recusa.data, form.avaria.data)
-            recebimento = Registro.criar_registro(id_nota_fiscal, current_user.id_responsavel, status)
-            db.session.add(recebimento)
+            # Busca as filiais associadas ao responsável logado
+            filiais_vinculadas = db.session.query(ResponsavelFilial).filter_by(id_responsavel=current_user.id_responsavel).all()
 
-            # Comita as alterações
-            db.session.commit()
+            if not filiais_vinculadas:
+                flash("Nenhuma filial vinculada ao responsável.", "warning")
+                form.filial.choices = []
+            else:
+                form.filial.choices = [("", "Selecione uma filial")] + [(filial.filial.name, filial.filial.value) for filial in filiais_vinculadas]
 
-            flash('Registro criado com sucesso!', 'success')
-            return redirect(url_for('tabela.tabela_notas_fiscais'))
+                # Lista de filiais vinculadas (extraindo o valor de 'filial' do enum)
+                filiais = [filial.filial for filial in filiais_vinculadas]
 
-        except ValueError as ve:
-            db.session.rollback()
-            flash(str(ve), "warning")
+                # Busca os responsáveis associados às mesmas filiais vinculadas ao responsável logado
+                responsaveis_vinculados = db.session.query(Responsavel).join(ResponsavelFilial).filter(ResponsavelFilial.filial.in_(filiais)).all()
+
+                if not responsaveis_vinculados:
+                    flash("Nenhum responsável encontrado nas filiais vinculadas.", "warning")
+                    form.id_responsavel.choices = []
+                else:
+                    # Preencher choices com os responsáveis vinculados
+                    form.id_responsavel.choices = [(0, "Selecione um responsável")] + [(resp.id_responsavel, resp.nome_responsavel) for resp in responsaveis_vinculados]
+
+            # Busca os centros
+            centros = [centro for centros in CENTROS_POR_FILIAL.values() for centro in centros]
+            if not centros:
+                form.nome_centro.choices = [("", "Selecione um centro")]
+            else:
+                form.nome_centro.choices = [("", "Selecione um centro")] + [(centro, centro) for centro in centros]
 
         except SQLAlchemyError as e:
-            db.session.rollback()
-            flash(f"Erro ao acessar o banco de dados: {str(e)}", "danger")
-
+            flash(f"Erro ao acessar o banco de dados ao carregar as empresas: {str(e)}", "danger")
+            form.nome_centro.choices = []
+            form.id_responsavel.choices = []
+            form.filial.choices = []
+            
         except Exception as e:
-            db.session.rollback()
-            flash(f"Erro inesperado: {str(e)}", "danger")
-    
-    return render_template('/nota_fiscal_e_recebimento/criar_nota_fiscal_e_recebimento.html', form=form, chave_acesso=chave_acesso, numero_nf=obter_numero_nf(chave_acesso))
+            flash(f"Erro inesperado ao carregar as opções: {str(e)}", "danger")
+            form.nome_centro.choices = []
+            form.id_responsavel.choices = []
+            form.filial.choices = []
+        
+
+        if form.validate_on_submit():
+            try:
+                nova_nota_fiscal = NotaFiscal.criar_nota_fiscal(chave_acesso, form)
+                db.session.add(nova_nota_fiscal)
+                db.session.flush()  # Envia as mudanças para o banco para gerar o ID
+                
+                # Acessa o ID da nova nota fiscal corretamente
+                id_nota_fiscal = nova_nota_fiscal.id_nota_fiscal
+                
+                # Cria uma nova instância de Registro
+                status = definicao_status_recebimento(form.recusa.data, form.avaria.data)
+                recebimento = Registro.criar_registro(id_nota_fiscal, current_user.id_responsavel, status)
+                db.session.add(recebimento)
+
+                # Comita as alterações
+                db.session.commit()
+
+                flash('Registro criado com sucesso!', 'success')
+                return redirect(url_for('tabela.tabela_notas_fiscais'))
+
+            except ValueError as ve:
+                db.session.rollback()
+                flash(str(ve), "warning")
+
+            except SQLAlchemyError as e:
+                db.session.rollback()
+                flash(f"Erro ao acessar o banco de dados: {str(e)}", "danger")
+
+            except Exception as e:
+                db.session.rollback()
+                flash(f"Erro inesperado: {str(e)}", "danger")
+        
+        return render_template('/nota_fiscal_e_recebimento/criar_nota_fiscal_e_recebimento.html', form=form, chave_acesso=chave_acesso, numero_nf=obter_numero_nf(chave_acesso))
