0f864f89a9bb256a8b021f8cf0ecaf846691cd4e
Vitor Oliveira <vitorsimon04@gmail.com>
2024-10-15 21:15:31 -0300

    FIX - Correção de redirecionamento após validação no MENU

--- RECEBIMENTO/routes/menu_routes.py
+++ RECEBIMENTO/routes/menu_routes.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template, flash, request
+import werkzeug
+from flask import Blueprint, render_template, redirect, url_for, flash, request
 from sqlalchemy.exc import SQLAlchemyError
 from RECEBIMENTO.forms import ChaveAcessoForm
 from RECEBIMENTO.utils import operacao_recebimento, operacao_mudar_status, operacao_estorno
@@ -21,17 +22,44 @@ def menu():
             chave_acesso = form.chave_acesso.data
             
             # Verifica se a operação escolhida
+            # Recebimento
             if acao == "registrar":
-                # Recebimento
-                return operacao_recebimento(chave_acesso)
+                # Valida a chave de acesso para a operação de mudança de status
+                validacao = operacao_recebimento(chave_acesso)
+
+                # Verifica se a função retorna um redirecionamento
+                if isinstance(validacao, werkzeug.wrappers.Response):
+                    # Retorna o redirecionamento
+                    return validacao
+                else:
+                    # Opereção recebimento
+                    return redirect(url_for("nota_fiscal.criar_nota_fiscal", chave_acesso=chave_acesso))
             
+            # Mudar status
             elif acao == "mudar_status":
-                # Mudar status
-                return operacao_mudar_status(chave_acesso)
+                # Valida a chave de acesso para a operação de mudança de status
+                validacao = operacao_mudar_status(chave_acesso)
+                
+                # Verifica se a função retorna um redirecionamento
+                if isinstance(validacao, werkzeug.wrappers.Response):
+                    # Retorna o redirecionamento
+                    return validacao
+                else:
+                    # Opereção 'mudar status'
+                    return redirect(url_for("mudar_status.registro_mudar_status", id_nota_fiscal=validacao.id_nota_fiscal))
             
+            # Estorno
             elif acao == "estorno":
-                # Estorno
-                return operacao_estorno(chave_acesso)
+                # Valida a chave de acesso para a operação de estorno
+                validacao = operacao_estorno(chave_acesso)
+                
+                # Verifica se a função retorna um redirecionamento
+                if isinstance(validacao, werkzeug.wrappers.Response):
+                    # Retorna o redirecionamento
+                    return validacao
+                else:
+                    # Operação de estorno
+                    return redirect(url_for("estorno.registro_estorno", id_nota_fiscal=validacao.id_nota_fiscal))
         
         except ValueError as ve:
             flash(str(ve), "warning")
