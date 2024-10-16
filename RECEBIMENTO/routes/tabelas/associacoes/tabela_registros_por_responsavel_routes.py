fc4c4ce5f8ee202e94dacf0dd528bc801a61f390
Vitor Oliveira <vitorsimon04@gmail.com>
2024-10-15 22:31:16 -0300

    FIX - Consulta para trazer sempre o último status do registro por Responsável

--- RECEBIMENTO/routes/tabelas/associacoes/tabela_registros_por_responsavel_routes.py
+++ RECEBIMENTO/routes/tabelas/associacoes/tabela_registros_por_responsavel_routes.py
@@ -1,5 +1,6 @@
 from flask import render_template, request
 from RECEBIMENTO import db
+from sqlalchemy import func
 from flask_login import login_required
 from RECEBIMENTO.models import Responsavel, Registro
 from . import associacoes_bp
@@ -13,11 +14,18 @@ def tabela_registros_por_responsavel(id_responsavel):
     # Paginação
     page = request.args.get('page', 1, type=int)
     per_page = request.args.get('per_page', 5, type=int)
-    
-    # Consulta para trazer todos os registros vinculados ao id_responsavel
+
+    # Subconsulta para encontrar o último registro por id_nota_fiscal
+    subquery = db.session.query(
+        Registro.id_nota_fiscal,
+        func.max(Registro.data_criacao).label('max_data_criacao')
+    ).group_by(Registro.id_nota_fiscal).subquery()
+
+    # Consulta principal para trazer os registros mais recentes por id_nota_fiscal
     registros = db.session.query(Registro)\
-        .filter_by(id_responsavel=responsavel.id_responsavel)\
+        .join(subquery, (Registro.id_nota_fiscal == subquery.c.id_nota_fiscal) & (Registro.data_criacao == subquery.c.max_data_criacao))\
+        .filter(Registro.id_responsavel == id_responsavel)\
         .order_by(Registro.data_criacao.desc())\
         .paginate(page=page, per_page=per_page, error_out=False)
-    
+
     return render_template('/tabelas/associacoes/tabela_registros_por_responsavel.html', registros=registros, id_responsavel=responsavel.id_responsavel)
