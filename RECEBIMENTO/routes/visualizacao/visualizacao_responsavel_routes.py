fade8aee9992750ab4417074d34de3139e4361cf
Vitor Oliveira <vitorsimon04@gmail.com>
2024-10-16 19:38:23 -0300

    FEAT - Exibição de filiais associadas em Visualização de responsável

--- RECEBIMENTO/routes/visualizacao/visualizacao_responsavel_routes.py
+++ RECEBIMENTO/routes/visualizacao/visualizacao_responsavel_routes.py
@@ -8,4 +8,5 @@ from . import visualizacao_bp
 @login_required
 def visualizar_responsavel(id_responsavel):
     responsavel = Responsavel.query.get_or_404(id_responsavel)
-    return render_template('visualizacao/visualizar_responsavel.html', responsavel=responsavel)
+    filiais_associadas = responsavel.responsavel_filial
+    return render_template('visualizacao/visualizar_responsavel.html', responsavel=responsavel, filiais=filiais_associadas)
