1b3b183d871ab9486b2524c9ff697018c89e9637
Vitor Oliveira <vitorsimon04@gmail.com>
2024-10-16 00:29:45 -0300

    FEAT - Exportação Excel Responsáveis

--- RECEBIMENTO/routes/tabelas_excel/__init__.py
+++ RECEBIMENTO/routes/tabelas_excel/__init__.py
@@ -4,3 +4,4 @@ tabelas_excel_bp = Blueprint('tabelas_excel', __name__, url_prefix="/tabelas_exc
 
 
 from .excel_notas_fiscais_routes import *
+from .excel_responsaveis_routes import *
--- RECEBIMENTO/routes/tabelas_excel/excel_responsaveis_routes.py
+++ RECEBIMENTO/routes/tabelas_excel/excel_responsaveis_routes.py
@@ -3,27 +3,24 @@ import io
 import pandas as pd
 from flask_login import login_required
 from RECEBIMENTO import db
-from RECEBIMENTO.models import NotaFiscal
+from RECEBIMENTO.models import Responsavel
 from . import tabelas_excel_bp
 
 
-@tabelas_excel_bp.route('/exportar/notas_fiscais')
+@tabelas_excel_bp.route('/exportar/responsaveis')
 @login_required
-def excel_notas_fiscais():
-    # Consulta para trazer as notas fiscais
-    notas_fiscais = db.session.query(NotaFiscal).all()
+def excel_responsaveis():
+    # Consulta para trazer os responsáveis
+    responsaveis = db.session.query(Responsavel).all()
 
     # Criar uma lista de dicionários com os dados para o DataFrame
     data = [{
-        'ID': nota_fiscal.id_nota_fiscal,
-        'Chave de acesso': nota_fiscal.chave_acesso,
-        'Nota Fiscal': nota_fiscal.numero_nf,
-        'Volumes': nota_fiscal.volumes,
-        'Código CTE': nota_fiscal.codigo_cte,
-        'CNPJ': nota_fiscal.cnpj,
-        'Filial': nota_fiscal.filial.value,
-        'Centro': nota_fiscal.nome_centro
-    } for nota_fiscal in notas_fiscais]
+        'ID': responsavel.id_responsavel,
+        'Nome': responsavel.nome_responsavel,
+        'E-mail': responsavel.email,
+        'Permissão': 'Administrador' if responsavel.permissao else 'Usuário',
+        'Status': 'Ativo' if responsavel.status else 'Inativo'
+    } for responsavel in responsaveis]
 
     # Criar um DataFrame usando pandas
     df = pd.DataFrame(data)
@@ -33,11 +30,11 @@ def excel_notas_fiscais():
     
     # Escrever o DataFrame no buffer como um arquivo Excel
     with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
-        df.to_excel(writer, index=False, sheet_name='Notas Fiscais')
+        df.to_excel(writer, index=False, sheet_name='Responsáveis')
     
     # Configurar o ponteiro do buffer para o início do arquivo
     output.seek(0)
 
     # Enviar o arquivo para o usuário
     return send_file(output, mimetype='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
-                     as_attachment=True, download_name='notas_fiscais.xlsx')
+                     as_attachment=True, download_name='responsaveis.xlsx')
