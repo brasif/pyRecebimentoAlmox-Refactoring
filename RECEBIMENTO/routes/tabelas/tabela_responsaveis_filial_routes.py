6628aed5b0b6af80464d532b5c628e3bce32ab1c
Vitor Oliveira <vitorsimon04@gmail.com>
2024-10-16 20:14:38 -0300

    FIX - Ordenação por nome da filial

--- RECEBIMENTO/routes/tabelas/tabela_responsaveis_filial_routes.py
+++ RECEBIMENTO/routes/tabelas/tabela_responsaveis_filial_routes.py
@@ -2,7 +2,6 @@ from flask import render_template, request
 from RECEBIMENTO import db
 from flask_login import login_required
 from RECEBIMENTO.models import ResponsavelFilial
-from sqlalchemy import desc
 from . import tabela_bp
 
 
@@ -13,9 +12,9 @@ def tabela_responsaveis_filial():
     page = request.args.get('page', 1, type=int)
     per_page = request.args.get('per_page', 5, type=int)
     
-    # Consulta para trazer os responsaveis
+    # Consulta para trazer os responsaveis, ordenando pelo nome da filial
     responsaveis_filial = db.session.query(ResponsavelFilial)\
-        .order_by(desc(ResponsavelFilial.data_alteracao))\
+        .order_by(ResponsavelFilial.filial)\
         .paginate(page=page, per_page=per_page, error_out=False)
     
     return render_template('/tabelas/tabela_responsaveis_filial.html', responsaveis_filial=responsaveis_filial)
