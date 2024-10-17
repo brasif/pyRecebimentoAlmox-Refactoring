424bd165efe54f2fb06f089e6fa84af8f0a3e947
Vitor Oliveira <vitorsimon04@gmail.com>
2024-10-17 13:57:05 -0300

    FEAT - Exibição de centros por filial em Menu_Filial

--- RECEBIMENTO/routes/gerenciamento_filial/menu_filial_routes.py
+++ RECEBIMENTO/routes/gerenciamento_filial/menu_filial_routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, abort
 from flask_login import login_required
-from RECEBIMENTO.models import Filiais
+from RECEBIMENTO.models import Filiais, CENTROS_POR_FILIAL
 from . import gerenciamento_filial_bp
 
 
@@ -12,5 +12,8 @@ def menu_filial(filial):
         filial_enum = Filiais[filial]  # Tenta obter a filial do Enum
     except KeyError:
         abort(404)  # Se não encontrar, retorna erro 404
-    
-    return render_template("/gerenciamento_filial/menu_filial.html", filial=filial_enum)
+
+    # Busca os centros associados à filial no dicionário
+    centros = CENTROS_POR_FILIAL.get(filial_enum, [])
+
+    return render_template("/gerenciamento_filial/menu_filial.html", filial=filial_enum, centros=centros)
