b4133156ceb999ceea338dbf7944c0f94bb3461f
Vitor Oliveira <vitorsimon04@gmail.com>
2024-10-18 13:05:20 -0300

    FEAT - Filtro a partir do HTML

--- RECEBIMENTO/routes/tabelas/tabela_responsaveis_filial_routes.py
+++ RECEBIMENTO/routes/tabelas/tabela_responsaveis_filial_routes.py
@@ -1,7 +1,7 @@
 from flask import render_template, request
-from RECEBIMENTO import db
 from flask_login import login_required
-from RECEBIMENTO.models import ResponsavelFilial
+from RECEBIMENTO.models import ResponsavelFilial, Responsavel
+from RECEBIMENTO.utils import responsaveis_filial_filtro
 from . import tabela_bp
 
 
@@ -11,10 +11,21 @@ def tabela_responsaveis_filial():
     # Paginação
     page = request.args.get('page', 1, type=int)
     per_page = request.args.get('per_page', 5, type=int)
-    
-    # Consulta para trazer os responsaveis, ordenando pelo nome da filial
-    responsaveis_filial = db.session.query(ResponsavelFilial)\
-        .order_by(ResponsavelFilial.filial)\
+
+    # Chama a função de filtro de responsáveis por filial com os parâmetros da requisição
+    query = responsaveis_filial_filtro(
+        ResponsavelFilial,
+        Responsavel,
+        request.args.get('filial', None), 
+        request.args.get('nome', None), 
+        request.args.get('email', None), 
+        request.args.get('permissao', None), 
+        request.args.get('status', None)
+    )
+
+    # Ordenação por nome da filial e do responsável
+    responsaveis_filial = query\
+        .order_by(ResponsavelFilial.filial.asc())\
         .paginate(page=page, per_page=per_page, error_out=False)
-    
+
     return render_template('/tabelas/tabela_responsaveis_filial.html', responsaveis_filial=responsaveis_filial)
