6ecb4d98217893a65593d7cfd61d28b2a17a766d
Vitor Oliveira <vitorsimon04@gmail.com>
2024-10-18 13:05:14 -0300

    FEAT - Filtro a partir do HTML

--- RECEBIMENTO/routes/tabelas/tabela_responsaveis_routes.py
+++ RECEBIMENTO/routes/tabelas/tabela_responsaveis_routes.py
@@ -1,8 +1,7 @@
 from flask import render_template, request
-from RECEBIMENTO import db
 from flask_login import login_required, current_user
 from RECEBIMENTO.models import Responsavel
-from sqlalchemy import desc
+from RECEBIMENTO.utils import responsaveis_filtro
 from . import tabela_bp
 
 
@@ -12,10 +11,19 @@ def tabela_responsaveis():
     # Paginação
     page = request.args.get('page', 1, type=int)
     per_page = request.args.get('per_page', 5, type=int)
-    
-    # Consulta para trazer os responsaveis
-    responsaveis = db.session.query(Responsavel)\
-        .order_by(desc(Responsavel.nome_responsavel))\
+
+    # Chama a função de filtro de responsáveis por filial com os parâmetros da requisição
+    query = responsaveis_filtro(
+        Responsavel,
+        request.args.get('nome', None),
+        request.args.get('email', None),
+        request.args.get('permissao', None),
+        request.args.get('status', None)
+    )
+
+    # Ordenação por nome (A-Z)
+    responsaveis = query\
+        .order_by(Responsavel.nome_responsavel.asc())\
         .paginate(page=page, per_page=per_page, error_out=False)
     
     return render_template('/tabelas/tabela_responsaveis.html', responsaveis=responsaveis, id_responsavel=current_user.id_responsavel)
