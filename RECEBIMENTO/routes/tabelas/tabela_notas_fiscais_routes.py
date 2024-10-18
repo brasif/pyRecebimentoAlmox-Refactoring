90e8c04574621b4f616080209d464ff5001a90fd
Vitor Oliveira <vitorsimon04@gmail.com>
2024-10-18 14:06:26 -0300

    FEAT - Filtro de notas fiscais

--- RECEBIMENTO/routes/tabelas/tabela_notas_fiscais_routes.py
+++ RECEBIMENTO/routes/tabelas/tabela_notas_fiscais_routes.py
@@ -1,7 +1,7 @@
 from flask import render_template, request
-from RECEBIMENTO import db
 from flask_login import login_required
 from RECEBIMENTO.models import NotaFiscal
+from RECEBIMENTO.utils import notas_fiscais_filtro
 from sqlalchemy import desc
 from . import tabela_bp
 
@@ -12,10 +12,21 @@ def tabela_notas_fiscais():
     # Paginação
     page = request.args.get('page', 1, type=int)
     per_page = request.args.get('per_page', 5, type=int)
-    
-    # Consulta para trazer as notas fiscais
-    notas_fiscais = db.session.query(NotaFiscal)\
-        .order_by(desc(NotaFiscal.data_alteracao))\
+
+    # Chama a função de filtro de notas fiscais com os parâmetros da requisição
+    query = notas_fiscais_filtro(
+        NotaFiscal,
+        request.args.get('chave_acesso', None),
+        request.args.get('nota_fiscal', None),
+        request.args.get('cnpj', None),
+        request.args.get('filial', None),
+        request.args.get('centro', None),
+        request.args.get('prioridade', None)
+    )
+
+    # Ordenação por data de alteração e depois por filial
+    notas_fiscais = query\
+        .order_by(desc(NotaFiscal.data_alteracao), NotaFiscal.filial.asc())\
         .paginate(page=page, per_page=per_page, error_out=False)
     
     return render_template('/tabelas/tabela_notas_fiscais.html', notas_fiscais=notas_fiscais)
