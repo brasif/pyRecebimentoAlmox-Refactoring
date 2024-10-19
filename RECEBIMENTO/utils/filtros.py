590de35b8aa1a1d096f8f71eb39aabe71e622f85
Vitor Oliveira <vitorsimon04@gmail.com>
2024-10-19 00:33:36 -0300

    FEAT - Função para filtro de Todos os Registros

--- RECEBIMENTO/utils/filtros.py
+++ RECEBIMENTO/utils/filtros.py
@@ -101,3 +101,37 @@ def notas_fiscais_filtro(model, chave_acesso, nota_fiscal, cnpj, filial, centro,
         query = query.filter(model.prioridade == (prioridade.lower() == 'true'))
     
     return query
+
+
+# Filtro de todos os registros
+def todos_registros_filtro(model_nf, model_reg, model_resp, query, chave_acesso, nota_fiscal, filial, centro, status, prioridade, responsavel):
+    # Aplica filtro da chave de acesso se presente
+    if chave_acesso:
+        query = query.filter(model_nf.chave_acesso.ilike(f'%{chave_acesso}%'))
+
+    # Filtro da nota fiscal se presente
+    if nota_fiscal:
+        # Aplica filtro utilizando a propriedade numero_nf
+        query = query.filter(func.substr(model_nf.chave_acesso, 27, 34).ilike(f'%{nota_fiscal}%'))
+
+    # Aplica filtro de filial se presente
+    if filial:
+        query = query.filter(model_nf.filial == filial)
+
+    # Aplica filtro do centro se presente
+    if centro:
+        query = query.filter(model_nf.nome_centro.ilike(f'%{centro}%'))
+
+    # Aplica filtro de prioridade se presente
+    if prioridade:
+        query = query.filter(model_nf.prioridade == (prioridade == 'true'))
+
+    # Aplica filtro do status_registro se presente
+    if status:
+        query = query.filter(model_reg.status_registro.ilike(f'%{status}%'))
+
+    # Aplica filtro do responsável pela atualização se presente
+    if responsavel:
+        query = query.filter(model_resp.nome_responsavel.ilike(f'%{responsavel}%'))
+
+    return query
