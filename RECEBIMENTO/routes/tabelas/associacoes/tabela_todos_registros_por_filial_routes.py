9015e9d878994d65afad5c19704bb3b65faa6299
Vitor Oliveira <vitorsimon04@gmail.com>
2024-11-12 14:33:07 -0300

    FEAT - Filtro todos os registros por filial

--- RECEBIMENTO/routes/tabelas/associacoes/tabela_todos_registros_por_filial_routes.py
+++ RECEBIMENTO/routes/tabelas/associacoes/tabela_todos_registros_por_filial_routes.py
@@ -1,7 +1,8 @@
 from flask import render_template, abort, request
 from RECEBIMENTO import db
 from flask_login import login_required
-from RECEBIMENTO.models import Filiais, Registro, NotaFiscal
+from RECEBIMENTO.models import Filiais, Registro, NotaFiscal, Responsavel
+from RECEBIMENTO.utils import todos_registros_por_filial_filtro
 from . import associacoes_bp
 
 
@@ -17,11 +18,43 @@ def tabela_todos_registros_por_filial(filial):
     page = request.args.get('page', 1, type=int)
     per_page = request.args.get('per_page', 5, type=int)
 
+    # Obtém os filtros do formulário
+    mes = request.args.get('mes')
+    chave_acesso = request.args.get('chave_acesso')
+    nota_fiscal = request.args.get('nota_fiscal')
+    centro = request.args.get('centro')
+    status = request.args.get('status')
+    prioridade = request.args.get('prioridade')
+    responsavel = request.args.get('responsavel')
+    data_recebimento = request.args.get('data_recebimento')
+    data_guarda = request.args.get('data_guarda')
+
+
     # Consulta para trazer todos os registros por filial
-    registros = db.session.query(Registro)\
+    registros_query = db.session.query(Registro)\
         .join(NotaFiscal, Registro.id_nota_fiscal == NotaFiscal.id_nota_fiscal)\
         .filter(NotaFiscal.filial == filial_enum)\
-        .order_by(Registro.id_registro.desc())\
-        .paginate(page=page, per_page=per_page, error_out=False)
+        .order_by(Registro.id_registro.desc())
+
+
+    # Aplica filtros
+    registros_query = todos_registros_por_filial_filtro(
+        NotaFiscal,
+        Registro,
+        Responsavel,
+        registros_query,
+        mes,
+        chave_acesso,
+        nota_fiscal,
+        centro,
+        status,
+        prioridade,
+        responsavel,
+        data_recebimento,
+        data_guarda
+    )
+
+    # Pagina os resultados
+    registros = registros_query.paginate(page=page, per_page=per_page, error_out=False)
 
     return render_template('/tabelas/associacoes/tabela_todos_registros_por_filial.html', registros=registros, filial=filial_enum)
