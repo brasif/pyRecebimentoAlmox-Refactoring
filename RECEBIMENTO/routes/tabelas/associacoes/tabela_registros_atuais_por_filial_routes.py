7b8701c05656a4deed4ea6466e20134027dc2a45
Vitor Oliveira <vitorsimon04@gmail.com>
2024-11-12 14:18:30 -0300

    FEAT - Filtro registros atuais por filial

--- RECEBIMENTO/routes/tabelas/associacoes/tabela_registros_atuais_por_filial_routes.py
+++ RECEBIMENTO/routes/tabelas/associacoes/tabela_registros_atuais_por_filial_routes.py
@@ -2,7 +2,8 @@ from flask import render_template, abort, request
 from RECEBIMENTO import db
 from sqlalchemy import func
 from flask_login import login_required
-from RECEBIMENTO.models import Filiais, Registro, NotaFiscal
+from RECEBIMENTO.models import Filiais, Registro, NotaFiscal, Responsavel
+from RECEBIMENTO.utils import registros_atuais_por_filial_filtro
 from . import associacoes_bp
 
 
@@ -18,6 +19,18 @@ def tabela_registros_atuais_por_filial(filial):
     page = request.args.get('page', 1, type=int)
     per_page = request.args.get('per_page', 5, type=int)
 
+    # Obtém os filtros do formulário
+    mes = request.args.get('mes')
+    data_recebimento = request.args.get('data_recebimento')
+    chave_acesso = request.args.get('chave_acesso')
+    nota_fiscal = request.args.get('nota_fiscal')
+    centro = request.args.get('centro')
+    status = request.args.get('status')
+    data_guarda = request.args.get('data_guarda')
+    prioridade = request.args.get('prioridade')
+    responsavel = request.args.get('responsavel')
+
+
     # Subconsulta para encontrar o último registro por id_nota_fiscal
     subquery = db.session.query(
         Registro.id_nota_fiscal,
@@ -25,11 +38,31 @@ def tabela_registros_atuais_por_filial(filial):
     ).group_by(Registro.id_nota_fiscal).subquery()
 
     # Consulta principal para trazer os registros mais recentes por id_nota_fiscal e filtrar e filial
-    registros = db.session.query(Registro)\
+    registros_query = db.session.query(Registro)\
         .join(subquery, (Registro.id_nota_fiscal == subquery.c.id_nota_fiscal) & (Registro.data_criacao == subquery.c.max_data_criacao))\
         .join(NotaFiscal, Registro.id_nota_fiscal == NotaFiscal.id_nota_fiscal)\
         .filter(NotaFiscal.filial == filial_enum)\
-        .order_by(Registro.data_criacao.desc())\
-        .paginate(page=page, per_page=per_page, error_out=False)
+        .order_by(Registro.data_criacao.desc())
+
+
+    # Aplica filtros
+    registros_query = registros_atuais_por_filial_filtro(
+        NotaFiscal,
+        Registro,
+        Responsavel,
+        registros_query,
+        mes,
+        chave_acesso,
+        nota_fiscal,
+        centro,
+        status,
+        prioridade,
+        responsavel,
+        data_recebimento,
+        data_guarda
+    )
+    
+    # Pagina os resultados
+    registros = registros_query.paginate(page=page, per_page=per_page, error_out=False)
 
     return render_template('/tabelas/associacoes/tabela_registros_atuais_por_filial.html', registros=registros, filial=filial_enum)
