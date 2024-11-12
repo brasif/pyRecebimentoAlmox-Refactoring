8c34b8ae1f921a4e3c16d7c4d0254d0410c94b59
Vitor Oliveira <vitorsimon04@gmail.com>
2024-11-12 13:56:47 -0300

    FEAT - Filtro notas fiscais por filial

--- RECEBIMENTO/routes/tabelas/associacoes/tabela_notas_fiscais_por_filial_routes.py
+++ RECEBIMENTO/routes/tabelas/associacoes/tabela_notas_fiscais_por_filial_routes.py
@@ -2,6 +2,7 @@ from flask import render_template, abort, request
 from RECEBIMENTO import db
 from flask_login import login_required
 from RECEBIMENTO.models import NotaFiscal, Filiais
+from RECEBIMENTO.utils import notas_fiscais_por_filial_filtro
 from sqlalchemy import desc
 from . import associacoes_bp
 
@@ -16,12 +17,32 @@ def tabela_notas_fiscais_por_filial(filial):
 
     # Paginação
     page = request.args.get('page', 1, type=int)
-    per_page = request.args.get('per_page', 5, type=int)
+    per_page = request.args.get('per_page', 5, type=int) 
     
     # Consulta para trazer as notas fiscais por filial
-    notas_fiscais = db.session.query(NotaFiscal)\
+    notas_fiscais_query = db.session.query(NotaFiscal)\
         .filter_by(filial=filial_enum)\
-        .order_by(desc(NotaFiscal.data_alteracao))\
-        .paginate(page=page, per_page=per_page, error_out=False)
+        .order_by(desc(NotaFiscal.data_alteracao))
+    
+    # Obtém os filtros do formulário
+    chave_acesso = request.args.get('chave_acesso')
+    nota_fiscal = request.args.get('nota_fiscal')
+    cnpj = request.args.get('cnpj')
+    centro = request.args.get('centro')
+    prioridade = request.args.get('prioridade')
+    
+    # Chama a função de filtro de notas fiscais com os parâmetros da requisição
+    notas_fiscais_query = notas_fiscais_por_filial_filtro(
+        NotaFiscal,
+        notas_fiscais_query,
+        chave_acesso,
+        nota_fiscal,
+        cnpj,
+        centro,
+        prioridade
+    )
+    
+    # Pagina os resultados
+    notas_fiscais = notas_fiscais_query.paginate(page=page, per_page=per_page, error_out=False)
     
     return render_template('/tabelas/associacoes/tabela_notas_fiscais_por_filial.html', notas_fiscais=notas_fiscais, filial=filial_enum)
--- RECEBIMENTO/utils/filtros.py
+++ RECEBIMENTO/utils/filtros.py
@@ -298,3 +298,31 @@ def registros_atuais_por_responsavel_e_filial_filtro(model_nf, model_reg, query,
         query = query.filter(func.date(model_reg.data_guarda) == data_guarda)
 
     return query
+
+
+# Filtro de notas fiscais por filial
+def notas_fiscais_por_filial_filtro(model, query, chave_acesso, nota_fiscal, cnpj, centro, prioridade):
+
+    # Aplica filtro da chave de acesso se presente
+    if chave_acesso:
+        query = query.filter(model.chave_acesso.ilike(f'%{chave_acesso}%'))
+    
+    # Filtro da nota fiscal se presente
+    if nota_fiscal:
+        # Aplica o filtro sobre a substring específica da chave de acesso que corresponde ao número da NF
+        query = query.filter(func.substr(model.chave_acesso, 27, 34).ilike(f'%{nota_fiscal}%'))
+    
+    # Aplica filtro do cnpj se presente
+    if cnpj:
+        # Aplica o filtro sobre a substring específica da chave de acesso que corresponde ao CNPJ
+        query = query.filter(func.substr(model.chave_acesso, 6, 20).ilike(f'%{cnpj}%'))
+    
+    # Aplica filtro do centro se presente
+    if centro:
+        query = query.filter(model.nome_centro.ilike(f'%{centro}%'))    
+    
+    # Aplica filtro de prioridade se presente e não for o valor padrão
+    if prioridade and prioridade in ['true', 'false']:
+        query = query.filter(model.prioridade == (prioridade.lower() == 'true'))
+    
+    return query
