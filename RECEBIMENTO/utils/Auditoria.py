4f15a55b57358efd2eded88957a7c971be0bc73c
Vitor Oliveira <vitorsimon04@gmail.com>
2024-11-13 16:29:59 -0300

    FEAT - Função genérica para auditoria - DELETE

--- RECEBIMENTO/utils/Auditoria.py
+++ RECEBIMENTO/utils/Auditoria.py
@@ -1,4 +1,3 @@
-from RECEBIMENTO import db
 from RECEBIMENTO.models.tb_auditoria_models import Auditoria
 from sqlalchemy.orm import sessionmaker
 
@@ -33,12 +32,11 @@ def insert(model, connection, target):
         session.close()
 
 
-def update(model, connection, target):
+def update(model, connection, target, state):
     session = sessionmaker(bind=connection)()
     changes = []
 
     # Verifica as alterações para os atributos que precisam ser auditados
-    state = db.inspect(target)
     for attr in state.attrs:
         history = attr.load_history()
         if history.has_changes():
@@ -65,6 +63,41 @@ def update(model, connection, target):
             )
             session.add(auditoria)
         
+        session.commit()
+    except Exception as e:
+        session.rollback()
+        print(f"Erro ao salvar auditoria: {e}")
+    finally:
+        session.close()
+
+
+def delete(model, connection, target):
+
+    # Cria uma nova sessão para a auditoria
+    Session = sessionmaker(bind=connection)
+    session = Session()
+
+    try:
+
+        # Obtém o valor da chave primária (supondo que há apenas uma chave primária)
+        primary_key_column = list(model.__table__.primary_key.columns)[0].name
+        id_referencia = getattr(target, primary_key_column)
+
+        # Constrói a string 'valor_novo' com base nos atributos do objeto
+        valor_antigo = ', '.join(f"{coluna}: {getattr(target, coluna)}" for coluna in target.__table__.columns.keys())
+
+        # Registra as informações antigas na auditoria
+        auditoria = Auditoria(
+            tabela_referenciada=model.__tablename__,
+            id_referencia=id_referencia,
+            acao="DELETE",
+            coluna_alterada="Todos os campos",
+            valor_antigo=valor_antigo,
+            valor_novo="Registro excluído",
+            id_responsavel=target.id_responsavel
+        )
+
+        session.add(auditoria)
         session.commit()
     except Exception as e:
         session.rollback()
